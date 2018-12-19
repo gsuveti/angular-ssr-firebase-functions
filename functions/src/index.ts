3adafd909851b221c09c8e474e8ebf567cc7c890
George Suveti <george.suveti@irian.ro>
2018-12-19 20:27:03 +0200

    - firebase init - sst with firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,50 @@
+import * as functions from 'firebase-functions';
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import {enableProdMode} from '@angular/core';
+// Express Engine
+import {ngExpressEngine} from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+
+import * as express from 'express';
+import {join} from 'path';
+
+(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+// Faster server renders w/ Prod mode (dev mode never needed)
+enableProdMode();
+
+// Express server
+const app = express();
+
+const DIST_FOLDER = join(process.cwd(), 'lib/browser');
+
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./server/main');
+
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
+
+
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
+
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
+// Server static files from /browser
+app.get('*.*', express.static(DIST_FOLDER));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+  res.render('index', {req});
+});
+
+
+exports.api = functions.https.onRequest(app);
